1649fdf54e46ffea53bbe94f6a5c5cca2f3cb513
Vladimir Sidorenko <vulkanfry@lunatic.cat>
2021-01-13 17:14:45 +0600

    added email and tg

--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -4,18 +4,9 @@ import Resume from "../../resume.json";
 
 function Hero() {
   return (
-    <section className="hero is-dark is-fullheight has-bg-image">
+    <section className="hero">
       <div className="hero-head">
         <NavBar />
-      </div>
-      <div className="hero-body">
-        <div className="container">
-          <p className="subtitle is-5 has-text-black has-text-weight-light">I'm a</p>
-          <h1 className="title has-text-black">{Resume.basics.label}</h1>
-          <h2 className="subtitle has-text-black">
-            {Resume.basics.location.region}, {Resume.basics.location.country}
-          </h2>
-        </div>
       </div>
       <div className="hero-foot" style={{ paddingBottom: "20px" }}>
         <div className="columns is-mobile">
