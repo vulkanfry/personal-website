f9df09f0bfd828f982f3674383d19842e8285cd8
Vladimir <vulkan.u4u1@gmail.com>
2021-01-29 14:03:51 +0600

    Update OSS.js

--- src/components/sections/OSS.js
+++ src/components/sections/OSS.js
@@ -24,7 +24,7 @@ class OSS extends Component {
       })
       .then(data => {
         let articles = [];
-        data = data.slice(0, 4);
+        data = _.sortBy(data, [function(o) { return o.created_at; }]).slice(0, 4);
         data.forEach((element, index) => {
           articles.push(
             <div className="column" key={index}>
